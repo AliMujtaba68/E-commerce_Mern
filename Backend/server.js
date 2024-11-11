8200f449981589c335876dc92f0b812de1d7e074
IhtishamUH <contactaesham@gmail.com>
2024-11-11 11:17:32 +0500

    User Model and Product Model done !

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import mongoose from 'mongoose';
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 
-// App config
-const app = express();
-const port = process.env.PORT || 4000;
+
+// App Config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 
-// middleware
-app.use(express.json());
-app.use(cors());
+// middlewares
+app.use(express.json())
+app.use(cors())
 
 
-// api endpoints
+//api endpoints
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hallllloooooo');
-});
+    res.send("API Working")
+})
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log('server started on PORT : ' + port))
