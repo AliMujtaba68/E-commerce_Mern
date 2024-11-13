b8cd93d94accaabe58ebef67b0226afd74549a91
Alimujtaba68 <a.mujtaba068@gmail.com>
2024-11-13 20:33:39 +0500

    Trying file uploads to cloud using curl

--- Backend/middleware/multer.js
+++ Backend/middleware/multer.js
@@ -1,14 +1,18 @@
 import multer from "multer";
 
-
+// Set up disk storage for images
 const storage = multer.diskStorage({
-    filename: function (req, file, callback) {
-        callback(null, file.originalname)
-    }
+  filename: function (req, file, callback) {
+    callback(null, file.originalname); // Keep the original file name
+  },
 });
 
+// Initialize Multer with storage configuration and fields for multiple images
+const upload = multer({ storage }).fields([
+  { name: 'image1', maxCount: 1 },
+  { name: 'image2', maxCount: 1 },
+  { name: 'image3', maxCount: 1 },
+  { name: 'image4', maxCount: 1 },
+]);
 
-const upload = multer({ storage })
-
-
-export default upload
+export default upload;
