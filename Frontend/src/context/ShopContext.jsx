a78d01ecf2331af92ad114bd9fec4dcb85864560
IhtishamUH <contactaesham@gmail.com>
2024-11-20 11:45:51 +0500

    From 9:24:00 To 9:34:00 this branch have some issues in Frontend Shopcontext getProductData().

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,18 +1,21 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets"; // Assuming products array is imported here
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";  // Import useNavigate
+import axios from 'axios'
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
+
     const navigate = useNavigate(); // Initialize navigate here
 
     const currency = '$';
     const delivery_fee = 10;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const [products, setProducts] = useState([]);
 
     // Add item to cart with the selected size
     const addToCart = async (itemId, size) => {
@@ -46,7 +49,7 @@ const ShopContextProvider = (props) => {
                     if (cartItems[items][item] > 0) {
                         totalCount += cartItems[items][item];
                     }
-                } catch (error) {}
+                } catch (error) { }
             }
         }
         return totalCount;
@@ -85,6 +88,31 @@ const ShopContextProvider = (props) => {
         console.log(cartItems);
     }, [cartItems]);
 
+    const getProductsData = async () => {
+
+        try {
+
+            const response = await axios.get(backendUrl + '/api/product/list')
+            if (response.data.success) {
+                setProducts(response.data.products)
+            }
+            else {
+                toast.error(response.data.message)
+            }
+
+
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+
+    }
+
+    useEffect(() => {
+        getProductsData()
+    }, [])
+
     const value = {
         products,
         currency,
@@ -98,7 +126,9 @@ const ShopContextProvider = (props) => {
         getCartCount,
         updateQuantity,
         getCartAmount,
-        navigate // Add navigate to context
+        navigate,
+        backendUrl
+
     };
 
     return (
